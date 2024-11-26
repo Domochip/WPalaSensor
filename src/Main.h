8a6f4d5b8b3c11d8d36908ef467bf241d30dd8e6
DomoChip <domochip@hotmail.com>
2024-11-26 19:44:33 +0100

    Remove DigiPots pins

--- src/Main.h
+++ src/Main.h
@@ -24,19 +24,11 @@
 
 #ifdef ESP8266
 
-// Pin 12, 13 and 14 are used by DigiPot Bus
-// Pins used for DigiPot Select
-#define MCP4151_5k_SSPIN D2
-#define MCP4151_50k_SSPIN D1
 // Pin for 1Wire DS18B20 bus
 #define ONEWIRE_BUS_PIN D4
 
 #else
 
-// Pin 19, 23 and 18 are used by DigiPot Bus
-// Pins used for DigiPot Select
-#define MCP4151_5k_SSPIN 21
-#define MCP4151_50k_SSPIN 22
 // Pin for 1Wire DS18B20 bus
 #define ONEWIRE_BUS_PIN 16
 
